0f125116e7e4cedc01da127e3ccb5aad47ed8533
Sohail khan <129187554+Sohailkhan2k02@users.noreply.github.com>
2024-05-21 23:21:11 +0530

    Create boilerplate.cpp

---
+++ boilerplate.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    int t; cin>>t;
+    while(t--){
+        
+    }
+}
